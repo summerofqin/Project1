f54e54173040e49d8d753734ee38548f172bfd45
summerofqin <109159178+summerofqin@users.noreply.github.com>
2023-01-20 14:06:02 +0800

    Add files via upload

---
+++ Project1/1_20.cpp
@@ -0,0 +1,27 @@
+﻿#include<stdio.h>
+#include<math.h>
+//法2：
+//这两个数中的总有一个数是小于等于两个数相乘的积的开根号
+//例如：36=4*9，4<开根号36=6
+int main() {
+	int count = 0;
+	int i = 0;
+
+	for (i = 100; i <= 200; i++) {
+		int flag = 1;
+		int j = 0;
+		for (j = 2; j <= sqrt(i); j++) {
+			if (i % j == 0) {
+				flag = 0;//不是素数
+				break;
+			}
+		}
+		if (flag == 1) {
+			count++;
+			printf("\n%d\n", i);
+		}
+	}
+	printf("count:%d", count);
+
+	return 0;
+}
